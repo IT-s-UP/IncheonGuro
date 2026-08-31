aac61bd530ae8eea4acb5183cb41588746a08c93
RyuJaeHwi <yujaehwi02@gmail.com>
2026-08-31 15:23:02 +0900

    Feat: 코스 상세 페이지 경로 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from '@/pages/MainPage/MainPage';
 import CourseRecommendPage from './pages/CourseRecommendPage/CourseRecommendPage';
 import CourseGuideListPage from '@/pages/CourseGuidePage/CourseGuideListPage';
+import CourseGuideDetailPage from '@/pages/CourseGuidePage/CourseGuideDetailPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-
       <Route path="/course-recommend" element={<CourseRecommendPage />} />
       <Route path="/course-guide" element={<CourseGuideListPage />} />
+      <Route path="/course-guide/:courseId" element={<CourseGuideDetailPage />} />
     </Routes>
   );
 }
