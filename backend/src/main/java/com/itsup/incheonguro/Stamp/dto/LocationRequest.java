b2b7e2ed1e8fa9d1c3af8367d6c7c85dc721f02a
Uyewon <dpdnns777@gmail.com>
2026-09-11 17:33:29 +0900

    Feat: 스탬프투어 dto 구현

---
+++ backend/src/main/java/com/itsup/incheonguro/Stamp/dto/LocationRequest.java
@@ -0,0 +1,16 @@
+package com.itsup.incheonguro.Stamp.dto;
+
+
+import lombok.Getter;
+
+
+@Getter
+public class LocationRequest {
+
+
+    private double latitude;
+
+
+    private double longitude;
+
+}
