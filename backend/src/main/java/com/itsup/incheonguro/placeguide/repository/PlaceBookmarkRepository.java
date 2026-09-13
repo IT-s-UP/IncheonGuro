3585a18253d08cced002653175249bb559a7840c
RyuJaeHwi <yujaehwi02@gmail.com>
2026-09-13 18:25:16 +0900

    Fix: 장소 안내 API repository 수정

--- backend/src/main/java/com/itsup/incheonguro/placeguide/repository/PlaceBookmarkRepository.java
+++ backend/src/main/java/com/itsup/incheonguro/placeguide/repository/PlaceBookmarkRepository.java
@@ -8,12 +8,12 @@ import java.util.Optional;
 // place_bookmark 테이블에 접근하는 리포지토리
 public interface PlaceBookmarkRepository extends JpaRepository<PlaceBookmark, Long> {
 
-  // 특정 사용자가 특정 장소를 북마크했는지 확인
-  Optional<PlaceBookmark> findByUserIdAndPlaceId(Long userId, Long placeId);
+  // 특정 사용자가 특정 콘텐츠(장소)를 북마크했는지 확인
+  Optional<PlaceBookmark> findByUserIdAndContentId(Long userId, String contentId);
 
-  // 특정 사용자가 특정 장소를 북마크했는지 여부만 true/false로 확인 -> swagger 확인용
-  boolean existsByUserIdAndPlaceId(Long userId, Long placeId);
+  // 특정 사용자가 특정 콘텐츠를 북마크했는지 여부만 true/false로 확인 -> swagger 확인용
+  boolean existsByUserIdAndContentId(Long userId, String contentId);
 
-  // 특정 사용자가 북마크한 모든 장소 조회 -> 북마크 탭에서 사용
+  // 특정 사용자가 북마크한 모든 콘텐츠 id 목록 조회 -> 북마크 탭에서 사용
   List<PlaceBookmark> findByUserId(Long userId);
 }
